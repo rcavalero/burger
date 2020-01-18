6e72ee1b505bc0ae9d37577aecc99b6fd9f5bd87
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-01-17 22:40:57 -0800

    add jawsDB info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "pass",
   database: "burger_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
